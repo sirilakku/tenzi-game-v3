d52c10e34bb368d4179895f0c920d57743de687e
Siri <sirilakku01@gmail.com>
2022-03-24 13:10:47 -0600

    Added p1 & p2

--- server.js
+++ server.js
@@ -5,28 +5,27 @@ const { welcome, displayName, playTenziFun, player } = require("./game");
 const { request } = require("express");
 const app = express();
 
+let p1 = player.player1.name
+let p2 = player.player2.name 
 app.get("/", welcome);
 
 app.get("/inputnames", (req, res) => {
-  player.player1.name = req.query.player1;
-  player.player2.name = req.query.player2;
+  p1 = req.query.player1;
+  p2 = req.query.player2;
 
-  if (player.player1.name && player.player2.name) {
+  if (p1 && p2) {
     res.send(
-      `${displayName(player.player1.name, player.player2.name)}. ${
-        player.player1.name
-      } is now Player1, ${
-        player.player2.name
-      } is now Player2. Please see "http://localhost:4000/instructions"`
+      `${displayName(p1, p2)}. ${p1} is now Player1, ${
+        p2} is now Player2. Please see "http://localhost:4000/instructions"`
     );
   } else if (
-    player.player1.name === undefined &&
-    player.player2.name === undefined
+    p1 === undefined &&
+    p2 === undefined
   ) {
     res.send(`No inputs provided. Please send player names!`);
   } else if (
-    player.player1.name === undefined ||
-    player.player2.name === undefined
+    p1 === undefined ||
+    p2 === undefined
   ) {
     res.send(
       `Only one player input provided. Please resubmit your post request with both player names`
@@ -41,19 +40,19 @@ app.get("/instructions", (req, res) => {
 });
 
 app.get("/playgame", (req, res) => {
-  if (player.player1.name) {
+  if (p1) {
     const countPlayer1 = playTenziFun();
     const countPlayer2 = playTenziFun();
 
     if (countPlayer1 < countPlayer2) {
       player.player1.score = player.player1.score + 1;
       res.send(
-        `Tenzi!!! ${player.player1.name} rolled ${countPlayer1} times and ${player.player2.name} rolled ${countPlayer2} times. To check the scores curl "http://localhost:4000/scores"`
+        `Tenzi!!! ${p1} rolled ${countPlayer1} times and ${p2} rolled ${countPlayer2} times. To check the scores curl "http://localhost:4000/scores"`
       );
     } else {
       player.player2.score = player.player2.score + 1;
       res.send(
-        `Tenzi!!! ${player.player1.name} rolled ${countPlayer1} times and ${player.player2.name} rolled ${countPlayer2} times.`
+        `Tenzi!!! ${p1} rolled ${countPlayer1} times and ${p2} rolled ${countPlayer2} times.`
       );
     }
   } else {
@@ -65,7 +64,7 @@ app.get("/playgame", (req, res) => {
 
 app.get("/scores", (req, res) => {
   res.send(
-    `Here are scores. ${player.player1.name} : ${player.player1.score} and ${player.player2.name} : ${player.player2.score}. To re-match "http://localhost:4000/restart" `
+    `Here are scores. ${p1} : ${player.player1.score} and ${p2} : ${player.player2.score}. To reset the game "http://localhost:4000/restart" or to play again "http://localhost:4000/playgame" `
   );
 });
 
